064b2581df68eaa4ff5236ac70204b0192965382
RigelCube <karolis@beawhale.io>
2026-02-23 01:34:55 +0200

    readd secure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -101,7 +101,7 @@ export function App() {
   useEffect(() => {
     // 1. Setup PeerJS for WebRTC
     const peer = new Peer({ host: RTC_SERVER_HOST, port: RTC_SERVER_PORT, 
-      secure: false
+      secure: true
     });
     peerRef.current = peer;
 
